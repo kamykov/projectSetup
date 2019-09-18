302d4a5f3737c0921a54ef81ef227f191e56c093
kamienieckik <kamienieckik@gmail.com>
2019-09-18 19:42:13 +0200

    PageSlider refactoring

--- Client/src/js/App.js
+++ Client/src/js/App.js
@@ -1,11 +1,7 @@
-import React, {
-  useContext, useState, useEffect,
-} from 'react';
+import React, { useContext } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { IntlProvider } from 'react-intl';
-import axios from 'axios';
 import translations from '../translations/langs';
-
 import { Context } from './context/storeContext';
 import MainWrapper from './hoc/MainWrapper/MainWrapper';
 import Header from './components/Header/Header';
@@ -13,66 +9,23 @@ import Menu from './components/Menu/Menu';
 import PageSlider from './components/PageSlider/PageSlider';
 import Profile from './components/Profile/Profile';
 import Login from './components/Login/Login';
-import Loading from './components/Loading/Loading';
 import Notifications from './components/Notifications/Notifications';
 
-import { storageAvailable } from './utils/helpers';
-import { SET_LANG, SET_DOTS } from './actions';
-
-const instance = axios.create({
-  baseURL: 'http://localhost:3000/',
-});
-
-
 export default function App() {
-  const [content, setContent] = useState(undefined);
   const { store, dispatch } = useContext(Context);
 
-  useEffect(() => {
-    if (storageAvailable('localStorage')) {
-      const lang = localStorage.getItem('lang');
-      const dots = localStorage.getItem('dots');
-      if (lang) dispatch({ type: SET_LANG, lang });
-      if (dots) dispatch({ type: SET_DOTS, value: dots });
-    }
-    return () => { };
-  }, []);
-
-  useEffect(() => {
-    instance
-      .get('/data')
-      .then((res) => {
-        setContent(res.data);
-      })
-      .catch((error) => console.log(error));
-
-    return () => { };
-  }, []);
-
   return (
-
-    <IntlProvider locale="pl" messages={translations.pl}>
+    <IntlProvider locale={store.lang} messages={translations.pl}>
       <Router>
         <Menu />
         <MainWrapper>
           <Header />
-          {content === undefined ? (
-            <Loading />
-          ) : (
-            <Switch>
-                <Route
-                  path="/slider"
-                  render={(props) => <PageSlider {...props} content={content} />}
-                />
-                <Route path="/login" render={() => <Login />} />
-                <Route path="/user/status" render={() => <Profile />} />
-                <Route path="not_found" render={() => <h1>OK</h1>} />
-                <Route
-                  path="/"
-                  render={(props) => <PageSlider {...props} content={content} />}
-                />
-              </Switch>
-          )}
+          <Switch>
+            <Route path="/login" component={Login} />
+            <Route path="/user/status" component={Profile} />
+            <Route path="/not_found" render={() => <h1>OK</h1>} />
+            <Route path="/" component={PageSlider} />
+          </Switch>
         </MainWrapper>
         <Notifications />
       </Router>
--- Client/src/js/components/PageSlider/PageSlider.jsx
+++ Client/src/js/components/PageSlider/PageSlider.jsx
@@ -1,65 +1,117 @@
-import React, { useContext } from 'react';
+import React, { useState, useEffect, useContext } from 'react';
+import PropTypes from 'prop-types';
 import { injectIntl } from 'react-intl';
 import { withRouter } from 'react-router-dom';
 import { Markup } from 'interweave';
+import axios from 'axios';
 import { Context } from '../../context/storeContext';
 import Headline from '../Headline/Headline';
 import Slide from '../Slide/Slide';
+import Loading from '../Loading/Loading';
 import './PageSlider.scss';
 import Logo from '../../../img/logo.svg';
+import { storageAvailable } from '../../utils/helpers';
 
-function PageSlider({ content, location: { hash }, intl }) {
+import { SET_LANG, SET_DOTS } from '../../actions';
+
+const instance = axios.create({
+  baseURL: 'http://localhost:3000/',
+});
+
+function PageSlider({ location: { hash }, intl }) {
   const {
-    store: { menu, lang },
+    store: { menu, lang }, dispatch,
   } = useContext(Context);
+
+  const [content, setContent] = useState([]);
+  const [body, setBody] = useState(undefined);
   const current = Math.max(
     0,
     menu.map((item) => item.link).indexOf(hash.substring(1)),
   );
   const sorted = menu.map((item) => content.find((page) => page.id === item.link));
 
-  const body = sorted.map((page, index) => {
-    const {
-      title, subtitle, headline, content, translation,
-    } = page;
-    const titleTranslation = intl.formatMessage({ id: translation });
-    const headlineTranslation = headline
-      ? intl.formatMessage({ id: headline })
-      : '';
-    const classes = [];
+  useEffect(() => {
+    if (storageAvailable('localStorage')) {
+      const lang = localStorage.getItem('lang');
+      const dots = parseInt(localStorage.getItem('dots'));
+      if (lang) dispatch({ type: SET_LANG, lang });
+      if (dots) dispatch({ type: SET_DOTS, value: dots });
+    }
+    return () => { };
+  }, []);
 
-    if (index === current) classes.push('active');
-    if (index === (current + 1) % sorted.length) classes.push('next');
-    if (index === (current === 0 ? sorted.length - 1 : current - 1)) { classes.push('previous'); }
+  useEffect(() => {
+    instance
+      .get('/data')
+      .then((res) => {
+        setContent(res.data);
+      })
+      .catch((error) => console.log(error));
 
-    return (
-      <div key={title} className={`page-slider__page ${classes.join(' ')}`}>
-        {index === current && (
-          <>
-            <h1 className="page__title">
-              {<Headline>{titleTranslation}</Headline>}
-            </h1>
-            <h2 className="page__subtitle">
-              {subtitle && <Headline>{subtitle}</Headline>}
-            </h2>
-            <h3 className="page__title">{headlineTranslation}</h3>
-            <div className="page__text">
-              <Slide delay={500}>
-                <Markup content={content[lang]} />
-              </Slide>
-            </div>
-          </>
-        )}
-      </div>
-    );
-  });
+    return () => {
+    };
+  }, []);
+
+  useEffect(() => {
+    if (content.length) {
+      const newBody = sorted.map((page, index) => {
+        const {
+          title, subtitle, headline, content, translation,
+        } = page;
+        const titleTranslation = intl.formatMessage({ id: translation });
+        const headlineTranslation = headline
+          ? intl.formatMessage({ id: headline })
+          : '';
+        const classes = [];
+
+        if (index === current) classes.push('active');
+        if (index === (current + 1) % sorted.length) classes.push('next');
+        if (index === (current === 0 ? sorted.length - 1 : current - 1)) { classes.push('previous'); }
+
+        return (
+          <div key={title} className={`page-slider__page ${classes.join(' ')}`}>
+            {index === current && (
+              <>
+                <h1 className="page__title">
+                  {<Headline>{titleTranslation}</Headline>}
+                </h1>
+                <h2 className="page__subtitle">
+                  {subtitle && <Headline>{subtitle}</Headline>}
+                </h2>
+                <h3 className="page__title">{headlineTranslation}</h3>
+                <div className="page__text">
+                  <Slide delay={500}>
+                    <Markup content={content[lang]} />
+                  </Slide>
+                </div>
+              </>
+            )}
+          </div>
+        );
+      });
+      setBody(newBody);
+    }
+    return () => { };
+  }, [content, hash]);
 
   return (
     <div className="page-slider">
       <Logo />
-      <div className="page-slider__content">{body}</div>
+      {body === undefined ? (
+        <Loading />
+
+      ) : (
+
+        <div className="page-slider__content">{body}</div>
+      )}
     </div>
   );
 }
+
+PageSlider.propTypes = {
+  location: PropTypes.node.isRequired,
+  intl: PropTypes.node.isRequired,
+};
 
 export default injectIntl(withRouter(PageSlider));
